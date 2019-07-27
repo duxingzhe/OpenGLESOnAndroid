41491c7134e3b833aa33472c44ce4657621a65d6
luxuan <myfather103@163.com>
2019-07-27 16:05:39 +0800

    提交库函数。

--- opengles-library/src/main/cpp/native_simple.h
+++ opengles-library/src/main/cpp/native_simple.h
@@ -2,7 +2,21 @@
 // Created by Luxuan on 2019/7/25.
 //
 
+#include <jni.h>
+
 #ifndef OPENGLESONANDROID_NATIVE_SIMPLE_H
 #define OPENGLESONANDROID_NATIVE_SIMPLE_H
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+JNIEXPORT void JNICALL surfaceCreated(JNIEnv *, jobject);
+JNIEXPORT void JNICALL surfaceChanged(JNIEnv *, jobject, jint, jint);
+JNIEXPORT void JNICALL onDrawFrame(JNIEnv *, jobject);
+
+#ifdef __cplusplus
+};
+#endif
 #endif //OPENGLESONANDROID_NATIVE_SIMPLE_H
