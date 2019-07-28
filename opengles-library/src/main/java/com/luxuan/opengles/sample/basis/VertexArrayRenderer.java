eda0dec973eac24c85607f4ebeec8c683f054eab
luxuan <myfather103@163.com>
2019-07-28 17:00:12 +0800

    提交库函数。

--- opengles-library/src/main/java/com/luxuan/opengles/sample/basis/VertexArrayRenderer.java
+++ opengles-library/src/main/java/com/luxuan/opengles/sample/basis/VertexArrayRenderer.java
@@ -1,8 +1,18 @@
 package com.luxuan.opengles.sample.basis;
 
+import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
 
+import com.luxuan.opengles.library.R;
+import com.luxuan.opengles.library.utils.ResReadUtils;
+import com.luxuan.opengles.library.utils.ShaderUtils;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
 
 public class VertexArrayRenderer implements GLSurfaceView.Renderer {
 
@@ -30,4 +40,49 @@ public class VertexArrayRenderer implements GLSurfaceView.Renderer {
     private int[] vaoIds=new int[1];
     private int[] vboIds=new int[1];
 
+    public VertexArrayRenderer(){
+        vertexBuffer= ByteBuffer.allocateDirect(vertexPoints.length*4).order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+
+        vertexBuffer.put(vertexBuffer);
+        vertexBuffer.position(0);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config){
+        GLES30.glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
+
+        final int vertexShaderId= ShaderUtils.compileVertexShader(ResReadUtils.readResource(R.raw.vertex_array_shader));
+        final int fragmentShaderId=ShaderUtils.compileFragmentShader(ResReadUtils.readResource(R.raw.fragment_array_shader));
+
+        mProgram=ShaderUtils.linkProgram(vertexShaderId, fragmentShaderId);
+
+        GLES30.glGenVertexArrays(1, vaoIds, 0);
+        GLES30.glBindVertexArray(vaoIds[0]);
+
+        GLES30.glGenBuffers(1, vboIds,0);
+        GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, vboIds[0]);
+        GLES30.glBufferData(GLES30.GL_ARRAY_BUFFER, vertexPoints.length*4, vertexBuffer, GLES30.GL_STATIC_DRAW);
+
+        GLES30.glVertexAttribPointer(VERTEX_POS_INDEX, VERTEX_POS_SIZE, GLES30.GL_FLOAT, false, VERTEX_STRIDE, 0);
+
+        GLES30.glBindVertexArray(0);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height){
+        GLES30.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl){
+        GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
+
+        GLES30.glUseProgram(mProgram);
+        GLES30.glBindVertexArray(vaoIds[0]);
+
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 0, 3);
+
+        GLES30.glBindVertexArray(0);
+    }
 }
