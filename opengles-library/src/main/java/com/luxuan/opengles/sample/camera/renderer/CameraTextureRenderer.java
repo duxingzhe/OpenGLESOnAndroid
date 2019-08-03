bb47b9870a16e3b961822c7a0a4432943f3a4cb0
Lu Xuan <myfather103@163.com>
2019-08-03 08:15:41 +0800

    提交库函数代码。

--- opengles-library/src/main/java/com/luxuan/opengles/sample/camera/renderer/CameraTextureRenderer.java
+++ opengles-library/src/main/java/com/luxuan/opengles/sample/camera/renderer/CameraTextureRenderer.java
@@ -1,5 +1,8 @@
 package com.luxuan.opengles.sample.camera.renderer;
 
+import android.graphics.SurfaceTexture;
+import android.opengl.GLES11Ext;
+import android.opengl.GLES20;
 import android.opengl.GLES30;
 
 import com.luxuan.opengles.library.R;
@@ -15,7 +18,7 @@ public class CameraTextureRenderer implements ITextureRenderer {
 
     private FloatBuffer mVertexBuffer;
 
-    private int mOESTexture=-1;
+    private int mOESTextureId=-1;
 
     private int mShaderProgram=-1;
 
@@ -49,7 +52,7 @@ public class CameraTextureRenderer implements ITextureRenderer {
     private float[] transformMatrix=new float[16];
 
     public CameraTextureRenderer(int OESTextureId){
-        mOESTexture=OESTextureId;
+        mOESTextureId=OESTextureId;
         mVertexBuffer=loadVertexBuffer(VERTEX_DATA);
     }
 
@@ -72,5 +75,35 @@ public class CameraTextureRenderer implements ITextureRenderer {
         aTextureCoordLocation= GLES30.glGetAttribLocation(mShaderProgram, CameraTextureRenderer.TEXTURE_COORD_ATTRIBUTE);
         uTextureMatrixLocation= GLES30.glGetUniformLocation(mShaderProgram, CameraTextureRenderer.TEXTURE_MATRIX_UNIFORM);
         uTextureSamplerLocation=GLES30.glGetUniformLocation(mShaderProgram, CameraTextureRenderer.TEXTURE_SAMPLER_UNIFORM);
+    }
+
+    @Override
+    public void onSurfaceChanged(int width, int height){
+        GLES30.glViewport(0,0,width, height);
+    }
+
+    @Override
+    public void onDrawFrame(SurfaceTexture surfaceTexture){
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        GLES30.glUseProgram(mShaderProgram);
+
+        surfaceTexture.updateTexImage();
+        surfaceTexture.getTransformMatrix(transformMatrix);
+
+        GLES30.glActiveTexture(GLES30.GL_TEXTURE0);
+        GLES30.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, mOESTextureId);
+        GLES30.glUniform1i(uTextureSamplerLocation, 0);
+        GLES30.glUniformMatrix4fv(uTextureMatrixLocation, 1, false, transformMatrix,0);
+
+        mVertexBuffer.position(0);
+        GLES30.glEnableVertexAttribArray(aPositionLocation);
+        GLES30.glVertexAttribPointer(aPositionLocation, 2, GLES30.GL_FLOAT, false, STRIDE, mVertexBuffer);
+
+        mVertexBuffer.position(2);
+        GLES30.glEnableVertexAttribArray(aTextureCoordLocation);
+        GLES30.glVertexAttribPointer(aTextureCoordLocation, 2, GLES30.GL_FLOAT, false, STRIDE, mVertexBuffer);
+
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 0, 6);
     }
 }
