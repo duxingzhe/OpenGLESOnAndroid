07b04ee05a19f0a3819d34e4fd7a4955c284cfe9
Lu Xuan <myfather103@163.com>
2019-08-04 08:56:59 +0800

    提交库函数代码。

--- opengles-library/src/main/java/com/luxuan/opengles/sample/color/NativeColorRenderer.java
+++ opengles-library/src/main/java/com/luxuan/opengles/sample/color/NativeColorRenderer.java
@@ -2,9 +2,39 @@ package com.luxuan.opengles.sample.color;
 
 import android.opengl.GLSurfaceView;
 
-public class NativeColorRenderer extends GLSurfaceView.Renderer {
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class NativeColorRenderer implements GLSurfaceView.Renderer {
 
     static {
         System.loadLibrary("native-color");
+    }
+
+    public native void surfaceCreated(int color);
+
+    public native void surfaceChanged(int width, int height);
+
+    public native void onDrawFrame();
+
+    private int color;
+
+    public NativeColorRenderer(int color){
+        this.color=color;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config){
+        surfaceCreated(color);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height){
+        surfaceChanged(width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl){
+        onDrawFrame();
     }
 }
