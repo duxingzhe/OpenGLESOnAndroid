8ac35f6f4f7285e688977773ac32d9e48882c7d0
Lu Xuan <myfather103@163.com>
2019-08-07 07:11:58 +0800

    提交库代码。

--- opengles-library/src/main/java/com/luxuan/opengles/sample/nativewindow/NativeWindowSample.java
+++ opengles-library/src/main/java/com/luxuan/opengles/sample/nativewindow/NativeWindowSample.java
@@ -1,4 +1,12 @@
 package com.luxuan.opengles.sample.nativewindow;
 
 public class NativeWindowSample {
+
+    static{
+        System.loadLibrary("naitve-window");
+    }
+
+    public native void drawColor(Object surface, int color);
+
+    public native void drawBitmap(Object surface, Object bitmap);
 }
