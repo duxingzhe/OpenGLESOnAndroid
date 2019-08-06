d41a935e16687a0800550b0105495559dce82d53
Lu Xuan <myfather103@163.com>
2019-08-07 07:00:10 +0800

    提交库代码。

--- opengles-library/src/main/cpp/native_window.cpp
+++ opengles-library/src/main/cpp/native_window.cpp
@@ -53,3 +53,57 @@ void ThrowException(JNIEnv *env, const char *exception, const char *message)
         env->ThrowNew(clazz, message);
     }
 }
+
+void drawColor(JNIEnv *env, jobject obj, jobject surface, jint colorARGB)
+{
+    int alpha=(colorARGB>>24)&0xFF;
+    int red=(colorARGB>>16)&0xFF;
+    int green=(colorARGB>>8)&0xFF;
+    int blue=colorARGB & 0xFF;
+
+    int colorARGBR=(alpha<<24)|(blue<<16)|(green<<16)|red;
+
+    ANativeWindow *window=ANativeWindow_fromSurface(env, surface);
+    if(NULL==window)
+    {
+        ThrowException(env, "java/lang/RuntimeException", "unable to get native window");
+        return;
+    }
+
+    int32_t result=ANativeWindow_setBuffersGeometry(window, 640, 640, WINDOW_FORMAT_RGBA_8888);
+    if(result<0)
+    {
+        ThrowException(env, "java/lang/RuntimeException", "unable to buffers geometry");
+        ANativeWindow_release(window);
+        window=NULL;
+        return;
+    }
+    ANativeWindow_acquire(window);
+
+    ANativeWindow_Buffer buffer;
+    if(ANativeWindow_lock(window, &buffer, NULL)<0)
+    {
+        ThrowException(env, "java/lang/RuntimeException", "unable to lock native window");
+
+        ANativeWindow_release(window);
+        window=NULL;
+        return;
+    }
+
+    uint32_t *line=(uint32_t *)buffer.bits;
+    for(int y=0;y<buffer.height;y++)
+    {
+        for(int x=0;x<buffer.width;x++)
+        {
+            line[x]=colorARGBR;
+        }
+        line=line+buffer.stride;
+    }
+
+    if(ANativeWindow_unlockAndPost(window)<0)
+    {
+        ThrowException(env, "java/lang/RuntimeException", "unable to unlock and post to native window");
+    }
+
+    ANativeWindow_release(window);
+}
